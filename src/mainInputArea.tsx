e7f524e7ef9f0552d1886a1acf8007d80f42d448
DavidAtStuff <david.rhode@stuff.co.nz>
2023-02-17 06:32:33 +1300

    finalising grid

--- src/mainInputArea.tsx
+++ src/mainInputArea.tsx
@@ -1,7 +1,8 @@
 
 import * as React from 'react';
 import Button from '@mui/material/Button';
-import { TextField } from '@mui/material';
+import { Box, TextField } from '@mui/material';
+import { Mainbox } from './ui/mainbox';
 //import * as validation from './validation/validation';
 
 
@@ -24,32 +25,35 @@ const ArticleMapper = () => {
     };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <Box
+    className="main-box"
+    display="flex"
+    justifyContent="center"
+    alignItems="center"
+    minHeight="100vh"
+    sx={{ p: 2, border: '1px  grey' }}>
+      <form onSubmit={handleSubmit}>
       <div>
-        <label htmlFor="articleId">Article Id</label>
-        <div className="inputfiel">
-          <input
-          id="articleId"
-          type="text"
-          value={form.articleId}
-          onChange={handleChange}
-          />
-        </div>
-        <div>
+        <label htmlFor="articleId"></label>
+        <div className="text-input-field-1">
           <TextField
             required
             id="articleId"
             type="text"
-            label="Required"
+            label="Composer Id Input"
             defaultValue="Composer Id"
             value={form.articleId}
             onChange={handleChange}
-            className="text-input-field"
+            variant="standard"
           />
         </div>
       </div>
-      <Button variant="contained">Submit</Button>
-    </form>
+
+      <div className="text-input-button-1">
+        <Button variant="contained">Submit</Button>
+      </div>
+      </form>
+    </Box>
   );
 };
 
--- src/ui/topbar.tsx
+++ src/ui/topbar.tsx
@@ -1,12 +1,24 @@
 import React from "react";
+import NavigationIcon from '@mui/icons-material/Navigation';
+import { Grid } from "@mui/material";
 
 export function Topbar(root: any) {
   const element = (
     <div className="topbar">
-      <div className="topbar-icon"></div>
-      <div className="banner-title">
-        <p>Send article to Naviga</p>
-      </div>
+      <Grid container
+        direction="row"
+        justifyContent="center"
+        alignItems="center">
+
+        <Grid item>
+          <NavigationIcon className="topbar-icon"></NavigationIcon>
+        </Grid>
+        
+        <Grid item xs>
+          <p className="banner-title">Send article to Naviga</p>
+        </Grid>
+
+      </Grid>
     </div>
   );
   return element;
